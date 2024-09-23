8735c9a83771ed6a38de6c85ff028782d8b0ab71
moomint8 <office05053@gmail.com>
2024-09-24 04:24:12 +0900

    feat(auth): 회원가입 Controller 구현
    회원가입 Controller를 구현했습니다.

---
+++ src/main/java/moomint/toy/currency_exchange/user/domain/aggregate/vo/MessageResponseVO.java
@@ -0,0 +1,10 @@
+package moomint.toy.currency_exchange.user.domain.aggregate.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MessageResponseVO {
+    String message;
+}
