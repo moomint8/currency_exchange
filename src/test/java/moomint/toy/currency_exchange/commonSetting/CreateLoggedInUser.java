f8451769336ba3670b55e75d12905a062976424c
moomint8 <office05053@gmail.com>
2024-10-26 03:22:22 +0900

    test(auth): Test 계정 생성 및 로그인 공용 메소드 구현

---
+++ src/test/java/moomint/toy/currency_exchange/commonSetting/CreateLoggedInUser.java
@@ -0,0 +1,46 @@
+package moomint.toy.currency_exchange.commonSetting;
+
+import moomint.toy.currency_exchange.common.Exception.DuplicateException;
+import moomint.toy.currency_exchange.user.domain.repository.UserRepository;
+import moomint.toy.currency_exchange.user.dto.SignupDTO;
+import moomint.toy.currency_exchange.user.service.AuthService;
+import moomint.toy.currency_exchange.user.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreateLoggedInUser {
+
+    private final AuthService authService;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CreateLoggedInUser(AuthService authService, UserRepository userRepository) {
+        this.authService = authService;
+        this.userRepository = userRepository;
+    }
+
+    // 일반 회원 계정 세팅
+    public void settingNormal() throws DuplicateException {
+
+        // 생성 계정 정보
+        SignupDTO userDTO = new SignupDTO(TestUserInfo.USERNAME, TestUserInfo.PASSWORD, TestUserInfo.NAME, TestUserInfo.EMAIL);
+        signup(userDTO);
+
+        // 로그인
+        login(userDTO.username());
+    }
+
+    private void signup(SignupDTO userDto) throws DuplicateException { authService.signUp(userDto); }
+
+    private void login(String username) {
+        CustomUserDetailsService customUserDetailsService = new CustomUserDetailsService(userRepository);
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities()));
+    }
+}
