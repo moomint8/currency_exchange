7fc4c4f480d638d39dfefab3d90947545d38b992
moomint8 <office05053@gmail.com>
2024-11-13 19:01:27 +0900

    feat(account): 본인 계좌 전체 조회 기능 테스트 구현
    현재 로그인된 계정의 전체 계좌를 조회하는 기능 테스트를 구현했습니다.

--- src/main/java/moomint/toy/currency_exchange/account/service/AccountService.java
+++ src/main/java/moomint/toy/currency_exchange/account/service/AccountService.java
@@ -1,7 +1,11 @@
 package moomint.toy.currency_exchange.account.service;
 
+import moomint.toy.currency_exchange.account.dto.AccountDTO;
 import moomint.toy.currency_exchange.common.Exception.NotLoggedInException;
+
+import java.util.List;
 
 public interface AccountService {
     String createAccount(String currency) throws NotLoggedInException;
+    List<AccountDTO> getAllAccounts() throws NotLoggedInException;
 }
