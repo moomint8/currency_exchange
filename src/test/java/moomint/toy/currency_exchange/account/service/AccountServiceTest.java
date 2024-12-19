3a36e5d5879e2ac1a91d9a99e026de0f49551d7f
moomint8 <office05053@gmail.com>
2024-12-19 22:51:21 +0900

    fix: ActiveProfiles() 어노테이션 추가

--- src/test/java/moomint/toy/currency_exchange/account/service/AccountServiceTest.java
+++ src/test/java/moomint/toy/currency_exchange/account/service/AccountServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
@@ -16,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@ActiveProfiles("ci")
 @SpringBootTest
 @Transactional
 class AccountServiceTest {
